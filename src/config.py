13abdc4f4ad74c8a2fea592e6986ec4f16cb2074
BPYap <32807097+BPYap@users.noreply.github.com>
2019-06-05 22:14:00 +0800

    Add data directory

--- src/config.py
+++ src/config.py
@@ -4,3 +4,7 @@ ROOT_DIR = os.path.abspath(os.path.dirname(__file__)) + "/../"
 
 MODEL_DIR = ROOT_DIR + "model/"
 PRETRAINED_DIR = MODEL_DIR + "pretrained/"
+
+DATA_DIR = ROOT_DIR + "data/"
+SOURCE_CORPUS_PATH = DATA_DIR + "source.txt"
+DESTINATION_CORPUS_PATH = DATA_DIR + "destination.txt"
