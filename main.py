c9a752a5815e00058cbe8d7b9d4973b8fc62bf67
BPYap <32807097+BPYap@users.noreply.github.com>
2019-06-17 21:40:03 +0800

    Add option to select individual script to run

--- main.py
+++ main.py
@@ -30,14 +30,29 @@ def _run_python(script, arguments):
 
 
 if __name__ == '__main__':
+    valid_scripts = ['build_parallel', 'prepare_dataset', 'preprocess', 'train', 'translate', 'all']
     parser = argparse.ArgumentParser()
-    parser.add_argument("config")
+    parser.add_argument("config", help="Configuration file (.yml) for all scripts")
+    parser.add_argument("--script", "-s", default='all',
+                        help="Choose to run an individual script. Available options: "
+                             "{} (default: all)".format(valid_scripts))
     args = parser.parse_args()
+    if args.script != 'all' and args.script not in valid_scripts:
+        raise RuntimeError("Unknown script name: {}".format(args.script))
 
     configs = load_yaml_config(args.config)
 
-    _run_python("script/build_parallel.py", _filter_args(configs, 'parallel'))
-    _run_python("script/prepare_dataset.py", _filter_args(configs, 'prepare'))
-    _run_python("OpenNMT-py/preprocess.py", _filter_args(configs, 'onmt-preprocess'))
-    _run_python("OpenNMT-py/train.py", _filter_args(configs, 'onmt-train'))
-    _run_python("OpenNMT-py/translate.py", _filter_args(configs, 'onmt-translate'))
+    if args.script == 'all' or args.script == 'build_parallel':
+        _run_python("script/build_parallel.py", _filter_args(configs, 'parallel'))
+
+    if args.script == 'all' or args.script == 'prepare_dataset':
+        _run_python("script/prepare_dataset.py", _filter_args(configs, 'prepare'))
+
+    if args.script == 'all' or args.script == 'preprocess':
+        _run_python("OpenNMT-py/preprocess.py", _filter_args(configs, 'onmt-preprocess'))
+
+    if args.script == 'all' or args.script == 'train':
+        _run_python("OpenNMT-py/train.py", _filter_args(configs, 'onmt-train'))
+
+    if args.script == 'all' or args.script == 'translate':
+        _run_python("OpenNMT-py/translate.py", _filter_args(configs, 'onmt-translate'))
