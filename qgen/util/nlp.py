c9028213741aa2a35f9fe897c849fa334190bf7d
BPYap <32807097+BPYap@users.noreply.github.com>
2019-09-10 17:03:22 +0800

    Restructure out imt training component

--- qgen/util/nlp.py
+++ qgen/util/nlp.py
@@ -2,10 +2,8 @@ import string
 
 import en_core_web_md
 import spacy
-import wmd
 
 _spacy_cache = None
-_spacy_wmd_cache = None
 _coref_cache = None
 
 FAST_TOKENIZER = spacy.load('en_core_web_sm', disable=['tagger', 'parser', 'ner', 'textcat'])
@@ -94,16 +92,6 @@ def get_spacy_model():
     return _spacy_cache
 
 
-def _get_spacy_wmd_model():
-    global _spacy_wmd_cache
-    if _spacy_wmd_cache is None:
-        print("Initializing spaCy model with WMD pipeline...")
-        _spacy_wmd_cache = en_core_web_md.load()
-        _spacy_wmd_cache.add_pipe(wmd.WMD.SpacySimilarityHook(_spacy_wmd_cache), last=True)
-
-    return _spacy_wmd_cache
-
-
 def _get_coref_model():
     global _coref_cache
 
@@ -112,16 +100,6 @@ def _get_coref_model():
         _coref_cache = spacy.load('en_coref_md', disable=['ner', 'textcat'])
 
     return _coref_cache
-
-
-def get_word_mover_dist(sentence1, sentence2):
-    nlp = _get_spacy_wmd_model()
-
-    try:
-        docs = list(nlp.pipe([sentence1, sentence2]))
-        return docs[0].similarity(docs[1])
-    except RuntimeError:
-        return float('inf')
 
 
 def get_ngrams(sentence, n):
